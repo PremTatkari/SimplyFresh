9ce79cc4eff88b33286d48979acab6ba80507c07
divyesh0406 <divyeshrmistry@gmail.com>
2023-03-25 18:33:37 +0530

    Carousel

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
-
 import './App.css';
+import './cara.css';
 import Cards from './components/Cards';
-
+import Carousel from "./components/Carousel";
 
 function App() {
   return (
     <div>
+      <div><Carousel></Carousel></div>
+      
       <div><Cards></Cards></div>
       <div><Cards></Cards></div>
       <div><Cards></Cards></div>
