e85b041c2bf392d34195d02220c56ce2a250e812
Prem Tatkari <tatkariprem2002@gmail.com>
2023-03-25 18:56:30 +0530

    Connect backend to frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,33 @@
-
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 import Cards from './components/Cards';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Exp from './components/Exp';
 
 function App() {
+  const [produce, setProduce] = useState([
+    {
+      title: "ABC",
+      price: 12,
+      class: "Organic"
+    }
+  ]);
+
+  useEffect(() => {
+    axios
+    .get("http://localhost:5000/api/produce")
+    .then((res) => {
+      setProduce(res.data);
+    })
+    .catch((err) => {
+      console.log("Error");
+    });
+  }, []);
+
+  const produceList = produce.length === 0 ? "No produce" : produce.map((prod,key) => <Cards title={prod.title} price={prod.price} class={prod.class} key={key} />);
+
   return (
     <div class="grid grid-cols-4 gap-4">
-      
-        <div><Cards /></div>
-        <div><Cards /></div>
-        <div><Cards /></div>
-        <div><Cards /></div>
-        <div><Cards /></div>
-        <div><Cards /></div>
-        <div><Cards /></div>
-        <div><Cards /></div>
-        <div><Cards /></div>
-
-
-      
+      {produceList}
     </div>
   );
 }
