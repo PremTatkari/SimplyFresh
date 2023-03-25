d984cd53ddd038b6a500c23943d0ef48d6b1876f
unknown <tatkariprem2002@gmail.com>
2023-03-25 16:02:22 +0530

    Create model schema

---
+++ models/Produce.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const ProduceSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    price:{
+        type: Number,
+        required: true
+    },
+    class:{
+        type: String,
+        required: true
+    }
+});
