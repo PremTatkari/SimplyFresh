4b9983d8c00cb3815eb17b6b8653ce9692d04210
daniel-was-taken <danferreira376@gmail.com>
2023-03-25 18:55:38 +0530

    Carousel added to site

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,20 @@
 import './App.css';
 import './cara.css';
 import Cards from './components/Cards';
-import Carousel from "./components/Carousel";import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Carousel from "./components/Carousel"; import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Exp from './components/Exp';
 
 function App() {
   return (
-    <div class="grid grid-cols-4 gap-4">
-      
+    <div>
+      <div><Carousel></Carousel></div>
+      <div class="grid grid-cols-4 gap-4">
+
         <div><Cards /></div>
         <div><Cards /></div>
         <div><Cards /></div>
-        <div><Carousel></Carousel></div>
-      
-      <div><Cards /></div>
+
+        <div><Cards /></div>
         <div><Cards /></div>
         <div><Cards /></div>
         <div><Cards /></div>
@@ -21,7 +22,7 @@ function App() {
         <div><Cards /></div>
 
 
-      
+      </div>
     </div>
   );
 }
