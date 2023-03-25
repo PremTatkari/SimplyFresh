3cb1298bc3be3940b096d9b60b36b45a1d3620ac
divyesh0406 <divyeshrmistry@gmail.com>
2023-03-26 01:06:51 +0530

    added cart page

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ import Exp from './components/Exp';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import ProductRegistration from './pages/ProductRegistration';
+import Cart from './pages/Cart';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -19,6 +20,7 @@ export default function Main() {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/producePage" element={<Exp />} />
+          <Route path="/cart" element={<Cart />} />
           <Route path="/productRegistration" element={<ProductRegistration />} />
           <Route path="*" element={<h1>404</h1>} />
         </Routes>
