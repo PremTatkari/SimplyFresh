b3a72838fefd6cd4b9b7828baa6e96e7597d30de
daniel-was-taken <danferreira376@gmail.com>
2023-03-25 18:11:00 +0530

    Card updation

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
 
 import './App.css';
 import Cards from './components/Cards';
-
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Exp from './components/Exp';
 
 function App() {
   return (
-    <div>
-      <div><Cards></Cards></div>
-      <div><Cards></Cards></div>
-      <div><Cards></Cards></div>
+    <div class="grid grid-cols-4 gap-4">
+      
+        <div><Cards /></div>
+        <div><Cards /></div>
+        <div><Cards /></div>
+        <div><Cards /></div>
+        <div><Cards /></div>
+        <div><Cards /></div>
+        <div><Cards /></div>
+        <div><Cards /></div>
+        <div><Cards /></div>
+
+
+      
     </div>
   );
 }
